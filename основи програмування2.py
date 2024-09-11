7fcd5ff451acb630427f21b8c5b2d74b52893c90
katyssaa <katya.kitsakk@gmail.com>
2024-09-11 21:35:48 +0300

    Add files via upload

---
+++ основи програмування2.py
@@ -0,0 +1,52 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+5
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+6
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+8.9
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+6.7
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+11
+
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+2.2
+
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+53.400000000000006
+
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+1.34
+
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+1662773.2861734084
+>>> 
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+0.0
+>>> 
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+0.7000000000000002
+>>> 
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+11, 2.2, 53.4, 1.34, 1662773.28, 0.0, 0.70
+>>> 
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+7
+>>> 
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+2.2, 0.0, 0.70
+>>> 
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+11, 1662773.28, 53.4, 1.34, 2.2, 0.0, 0.70
+>>> 
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+Катерина Кіцак
+>>> 
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+291
+>>> 
+= RESTART: C:/Users/Катеріна/AppData/Local/Programs/Python/Python312/практична робота номер 1 кк.py
+Лабораторну роботу номер 1 виконала Катерина Кіцак, студентка групи 291.Висновок: на даній лабораторній роботі було проведено повторення та ознайомленя з основами мови програмування Python/Виконано базові дії над операторами та функціями даної мови програмування.Роботу виконано в повному обсязі.
